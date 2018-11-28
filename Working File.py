033bd5d7f3c714387c8769f1d57b670654050bcb
Raghav Mishra <mishraraghav24@gmail.com>
2018-11-29 09:30:49 +1000

    Slightly more refined data

--- Working File.py
+++ Working File.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import math as m
-import csv
+import pandas as pd
+
+data = pd.read_csv("refined.csv")
+print(data.head())
+
